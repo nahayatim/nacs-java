0add6ddc097c60cb7370fe0219478700de84795a
nahayatim <mahtabnahayati@gmail.com>
2019-01-11 14:46:38 +0100

    update testing

--- java-week8/src/reflection8/ThermostatTest.java
+++ java-week8/src/reflection8/ThermostatTest.java
@@ -1,15 +1,41 @@
 package reflection8;
 
 import ex04.PowerLevelScouter;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ThermostatTest {
 
-    private Thermostat thermostat = new Thermostat();
+    private Thermostat thermostat = new Thermostat(s -> s < 0, l -> l + "degrees Celsius");
+    private Thermostat thermostat2 = new Thermostat(s -> s >= 80, t -> t + 273.15 + "degrees kelvin");
 
     @Test
-    void sense() {
+    void skiing() {
+        Double temperture = 2.0;
+        Double temperture2 = -1.0;
+
+        String actual = thermostat.sense(temperture);
+        String expected = temperture + "degrees Celsius";
+
+        Assertions.assertEquals(expected, actual);
+
+        actual = thermostat.sense(temperture2);
+        expected = temperture2 + "degrees Celsius";
+
+    }
+
+    @Test
+        void testSauna(){
+            Double temprature = 79.0;
+            Double temprature2 = 80.0;
+
+        String actual = thermostat2.sense(temprature);
+        String expected=temprature+"degrees kelvin";
+
+        Assertions.assertEquals(expected,actual);
+
+
     }
 }
